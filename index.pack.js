317deb962911b288962c1b823efa2eb9d7799628
greerse14@gmail.com <140839981+SarahEGreer@users.noreply.github.com>
2024-08-26 13:36:22 -0400

    stored images locally. Fixed photo aspect ratio

--- index.pack.js
+++ index.pack.js
@@ -643,7 +643,7 @@ exports.default = [{
     googleMapsUrl: "https://maps.app.goo.gl/Z83ejv7h88EB2SJUA",
     startDate: "28 Dec, 2022",
     description: "The Biltmore Estate is a  Gilded Age mansion built by George Washington Vanderbilt II in the late 19th century, boasting luxurious interiors, vast gardens, and breathtaking mountain views, it is America's largest privately-owned home.",
-    imageUrl: "https://source.unsplash.com/k1HE3GHCxZE",
+    imageUrl: "images/biltmore.jpg",
     imageAltText: "Biltmore Estate conservatory with sky lights, greenery, and Christmas garlands."
 }, {
     id: 2,
@@ -652,7 +652,7 @@ exports.default = [{
     googleMapsUrl: "https://maps.app.goo.gl/CV2GJZXZ1zM9MoQG6",
     startDate: "5 Feb, 2019",
     description: "Jiufen is a historic mountain village said to have inspired Hayao Miyazaki's Spirited Away. It is known for its narrow alleyways, vibrant lanterns, and traditional teahouses.",
-    imageUrl: "https://source.unsplash.com/dClLlcwxRGw",
+    imageUrl: "images/jiufen.jpg",
     imageAltText: "Red lantern lit stairway full of people at night."
 }, {
     id: 3,
@@ -662,7 +662,7 @@ exports.default = [{
     startDate: "12 April, 2018",
     endDate: "14 April, 2018",
     description: "Tiger Leaping Gorge in China is one of the deepest and most dramatic river canyons globally, carved by the Jinsha River as it flows between towering cliffs, offering stunning views and trekking opportunities.",
-    imageUrl: "https://source.unsplash.com/XGK-__HBdiM",
+    imageUrl: "images/tiger-leaping-gorge.jpeg",
     imageAltText: "Bridge over a gorge with trees on either side."
 }, {
     id: 4,
@@ -671,7 +671,7 @@ exports.default = [{
     googleMapsUrl: "https://maps.app.goo.gl/yZsGRNeFNtgAhdHp9",
     startDate: "12 Jan, 2018",
     description: "At Batu Caves in Malaysia, you can explore a series of limestone caves adorned with Hindu shrines and statues, including the towering golden statue of Lord Murugan. Additionally, you'll encounter macaque monkeys, climb the steep staircase to the main cave entrance, and experience stunning views of the surrounding area from atop the caves.",
-    imageUrl: "https://source.unsplash.com/gcQkighWofc",
+    imageUrl: "images/batu-caves.jpg",
     imageAltText: "Colorful steps up to a cave with a giant gold statue of the Hindu god Murugan."
 }, {
     id: 5,
@@ -680,7 +680,7 @@ exports.default = [{
     googleMapsUrl: "https://maps.app.goo.gl/LqCr2JzRQqCrmcBK9",
     startDate: "30 Jan, 2018",
     description: "Wadjemup Island is located off the coast of Western Australia, renowned for its stunning beaches, crystal-clear waters, and marine life. It's also home to the quokka, a friendly marsupial found nowhere else in the world.",
-    imageUrl: "https://source.unsplash.com/6ZDY7XhT8_0",
+    imageUrl: "images/rottnest.jpg",
     imageAltText: "A quokka looking at the camera."
 }, {
     id: 6,
@@ -689,7 +689,7 @@ exports.default = [{
     googleMapsUrl: "https://maps.app.goo.gl/vx28JKQxjmfrgki48",
     startDate: "10 Dec, 2016",
     description: "Neuschwanstein Castle is a fairy-tale fortress renowned for its stunning architecture and picturesque surroundings. Commissioned by King Ludwig II in the 19th century, it embodies romanticism with its soaring towers, ornate interiors, and breathtaking views.",
-    imageUrl: "https://source.unsplash.com/IZunjrb3soo",
+    imageUrl: "images/neuschwanstein.jpg",
     imageAltText: "A white castle on a tree covered hill."
 }];
 
